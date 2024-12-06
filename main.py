176dd647c9af2b61661ee3e8cbdb49713d5d1aff
Antonych50 <Antonych50@gmail.com>
2024-12-06 13:06:15 +0300

    merge with branche "const'

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-pygame.display.set_caption(«Игра Тир»)
+pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/мишень.jpg")
 target_image = pygame.image.load("img/apple1.png")
 target_width = 80
