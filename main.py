dd2ce3bf5c74cd99bda4926719e1bbf459d16019
Antonych50 <Antonych50@gmail.com>
2024-12-07 13:03:33 +0300

    addin and show game score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from pygame.examples.moveit import HEIGHT
+import time
 
 pygame.init()
 SCREEN_WIDTH = 800
@@ -13,20 +13,29 @@ target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-
+#color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+color = (255,255,255)
+game_score = 0
+pygame.font.init()
+font = pygame.font.SysFont('Arial', 24)
 running = True
 while running:
     screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            continue
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                game_score+= 10
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            else:
+                game_score-= 10
     screen.blit(target_image, (target_x, target_y))
     pygame.display.update()
+text_surface = font.render("У Вас " + str(game_score) + "очков!", True, (0, 0, 255))
+screen.blit(text_surface, (100, 100))
+time.sleep(25)
 pygame.quit()
